cd409a5982725fff0e2c1283f117f3a908ff1b37
skwas <skwas@skwas.net>
2020-01-14 23:04:38 +0100

    Add configuraton extensions (see #8)

---
+++ test/IbanNet.Extensions.Bban.Tests/BbanConfigurationExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿#if NETCOREAPP3_0
+using System;
+using System.Linq;
+using FluentAssertions;
+using IbanNet.DependencyInjection.ServiceProvider;
+using IbanNet.Validation.Rules;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace IbanNet
+{
+    /// <summary>
+    /// Asserts rule is correctly registered using the builder extensions.
+    /// </summary>
+    public class BbanConfigurationExtensionsTests
+    {
+        private readonly IIbanValidator _validator;
+
+        public BbanConfigurationExtensionsTests()
+        {
+            IServiceProvider services = new ServiceCollection()
+                // Register rule
+                .AddIbanNet(builder => builder.ValidateNationalCheckDigits())
+                .BuildServiceProvider();
+
+            _validator = services.GetRequiredService<IIbanValidator>();
+        }
+
+        [Fact]
+        public void Given_that_rule_is_registered_it_should_be_added_to_options()
+        {
+            ((IbanValidator)_validator).Options.Rules
+                .OfType<HasValidNationalCheckDigitsRule>()
+                .Should()
+                .HaveCount(1);
+        }
+    }
+}
+#endif
