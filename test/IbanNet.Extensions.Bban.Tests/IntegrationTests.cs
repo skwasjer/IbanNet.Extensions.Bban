46f5d24dc85358d481f4dc0da95a5c98a3b960a5
8b3ca647b50710d503957eae9d63c92e066da08a <skwas@skwas.net>
2020-02-10 03:23:06 +0100

    Fix whitespace

--- test/IbanNet.Extensions.Bban.Tests/IntegrationTests.cs
+++ test/IbanNet.Extensions.Bban.Tests/IntegrationTests.cs
@@ -5,62 +5,62 @@ using Xunit;
 
 namespace IbanNet.Extensions.Bban
 {
-	public class IntegrationTests
-	{
-		private readonly IbanValidator _validator;
+    public class IntegrationTests
+    {
+        private readonly IbanValidator _validator;
 
         public IntegrationTests()
         {
             _validator = new IbanValidator(new IbanValidatorOptions
-			{
-				Rules =
-				{
-					new HasValidNationalCheckDigitsRule()
-				}
-			});
-		}
+            {
+                Rules =
+                {
+                    new HasValidNationalCheckDigitsRule()
+                }
+            });
+        }
 
         [Theory]
-		[InlineData("FR1420041010050500013M02606")]
-		[InlineData("FR7630006000011234567890189")]
-		[InlineData("MR1300020001010000123456753")]
-		[InlineData("MC5811222000010123456789030")]
-		[InlineData("IT60X0542811101000000123456")]
-		[InlineData("SM86U0322509800000000270100")]
-		[InlineData("NO9386011117947")]
-		[InlineData("BA391290079401028494")]
-		public void Given_iban_with_valid_national_check_digits_when_validating_it_should_validate(string ibanWithNationalCheckDigits)
-		{
-			string countryCode = ibanWithNationalCheckDigits.Substring(0, 2);
+        [InlineData("FR1420041010050500013M02606")]
+        [InlineData("FR7630006000011234567890189")]
+        [InlineData("MR1300020001010000123456753")]
+        [InlineData("MC5811222000010123456789030")]
+        [InlineData("IT60X0542811101000000123456")]
+        [InlineData("SM86U0322509800000000270100")]
+        [InlineData("NO9386011117947")]
+        [InlineData("BA391290079401028494")]
+        public void Given_iban_with_valid_national_check_digits_when_validating_it_should_validate(string ibanWithNationalCheckDigits)
+        {
+            string countryCode = ibanWithNationalCheckDigits.Substring(0, 2);
 
-			// Act
-			ValidationResult result = _validator.Validate(ibanWithNationalCheckDigits);
+            // Act
+            ValidationResult result = _validator.Validate(ibanWithNationalCheckDigits);
 
-			// Assert
-			result.Should().BeEquivalentTo(new ValidationResult
-			{
-				AttemptedValue = ibanWithNationalCheckDigits,
-				Country = _validator.SupportedCountries[countryCode]
-			});
-		}
+            // Assert
+            result.Should().BeEquivalentTo(new ValidationResult
+            {
+                AttemptedValue = ibanWithNationalCheckDigits,
+                Country = _validator.SupportedCountries[countryCode]
+            });
+        }
 
         [Theory]
-		[InlineData("FR4120041010050500013M02605")]
-		[InlineData("FR0630006000011234567890188")]
-		public void Given_iban_with_invalid_national_check_digits_when_validating_it_should_not_validate(string ibanWithTamperedNationalCheckDigits)
-		{
-			string countryCode = ibanWithTamperedNationalCheckDigits.Substring(0, 2);
+        [InlineData("FR4120041010050500013M02605")]
+        [InlineData("FR0630006000011234567890188")]
+        public void Given_iban_with_invalid_national_check_digits_when_validating_it_should_not_validate(string ibanWithTamperedNationalCheckDigits)
+        {
+            string countryCode = ibanWithTamperedNationalCheckDigits.Substring(0, 2);
 
-			// Act
-			ValidationResult result = _validator.Validate(ibanWithTamperedNationalCheckDigits);
+            // Act
+            ValidationResult result = _validator.Validate(ibanWithTamperedNationalCheckDigits);
 
-			// Assert
-			result.Should().BeEquivalentTo(new ValidationResult
-			{
+            // Assert
+            result.Should().BeEquivalentTo(new ValidationResult
+            {
                 AttemptedValue = ibanWithTamperedNationalCheckDigits,
-				Country = _validator.SupportedCountries[countryCode],
-				Error = new InvalidNationalCheckDigitsResult()
-			});
-		}
-	}
+                Country = _validator.SupportedCountries[countryCode],
+                Error = new InvalidNationalCheckDigitsResult()
+            });
+        }
+    }
 }
