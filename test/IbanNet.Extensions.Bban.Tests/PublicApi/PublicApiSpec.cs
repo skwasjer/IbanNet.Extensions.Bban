6a19cbff9fb1db14d29341a25d85347995f6d9b9
Martijn Bodeman <11424653+skwasjer@users.noreply.github.com>
2025-11-15 19:03:59 +0100

    test: verify the public API (#67)

---
+++ test/IbanNet.Extensions.Bban.Tests/PublicApi/PublicApiSpec.cs
@@ -0,0 +1,62 @@
+﻿#if VERIFY_PUBLIC_API
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using PublicApiGenerator;
+
+namespace IbanNet.Extensions.Bban.PublicApi;
+
+public abstract class PublicApiSpec
+{
+    private readonly Type _assemblyMarkerType;
+    private readonly string? _sourceFile;
+
+    private readonly ApiGeneratorOptions _options = new()
+    {
+        ExcludeAttributes =
+        [
+            typeof(InternalsVisibleToAttribute).FullName!
+        ],
+        DenyNamespacePrefixes = []
+    };
+
+    protected PublicApiSpec(Type assemblyMarkerType, [CallerFilePath] string? sourceFile = default)
+    {
+        _assemblyMarkerType = assemblyMarkerType;
+        _sourceFile = sourceFile;
+    }
+
+    [Fact]
+    [Trait("Category", "PublicApi")]
+    public Task Api_has_not_changed()
+    {
+        bool runningOnMono = Type.GetType("Mono.Runtime") != null;
+        if (runningOnMono)
+        {
+            return Task.CompletedTask;
+        }
+
+        Assembly sut = _assemblyMarkerType.Assembly;
+        VerifySettings settings = VerifyHelpers.GetDefaultSettings(_assemblyMarkerType);
+
+        // Act
+        string publicApi = sut.GeneratePublicApi(_options);
+
+        // Assert
+        // ReSharper disable once ExplicitCallerInfoArgument
+#pragma warning disable S3236
+        return Verify(publicApi, settings, _sourceFile!);
+#pragma warning restore S3236
+    }
+}
+#else
+namespace IbanNet.Extensions.Bban.PublicApi;
+
+// Empty shim for when verification is disabled.
+public abstract class PublicApiSpec
+{
+    // ReSharper disable once UnusedParameter.Local
+    protected PublicApiSpec(Type assemblyMarkerType)
+    {
+    }
+}
+#endif
