9607f245f4f66319da89cbf5c93f69dc42a808b9
Martijn Bodeman <11424653+skwasjer@users.noreply.github.com>
2025-11-17 08:33:41 +0100

    perf: add benchmark and improve mod97-10 (#71)
    * perf: replace the exception with an assert. This should not happen at runtime by consumers since consumers can not configure/alter the behavior.
    
    * perf: add benchmark and improve mod algorithm by accrueing as much digits as possible before integer overflow, and only then apply modulo. Mod on every iteration is much slower.

--- src/IbanNet.Extensions.Bban/CheckDigits/Algorithms/Mod9710Algorithm.cs
+++ src/IbanNet.Extensions.Bban/CheckDigits/Algorithms/Mod9710Algorithm.cs
@@ -1,4 +1,5 @@
-﻿using IbanNet.CheckDigits.Calculators;
+﻿using System.Diagnostics;
+using IbanNet.CheckDigits.Calculators;
 
 namespace IbanNet.Extensions.Bban.CheckDigits.Algorithms;
 
@@ -8,6 +9,10 @@ namespace IbanNet.Extensions.Bban.CheckDigits.Algorithms;
 /// <param name="complement">The complement to return.</param>
 internal sealed class Mod9710Algorithm(Complement complement) : ICheckDigitsCalculator
 {
+    /// <summary>
+    /// 8 because with 2 more digits added, we are risking integer overflow.
+    /// </summary>
+    private const int MaxDigitsBeforeIntegerOverflow = 8;
     private const int Modulo = 97;
     private readonly Complement _complement = complement ?? throw new ArgumentNullException(nameof(complement));
 
@@ -23,20 +28,43 @@ internal sealed class Mod9710Algorithm(Complement complement) : ICheckDigitsCalc
             throw new ArgumentNullException(nameof(value));
         }
 
+        int digits = 0;
         int remainder = 0;
         // ReSharper disable once ForCanBeConvertedToForeach - justification : performance
         for (int i = 0; i < value.Length; i++)
         {
             char ch = value[i];
             int number = AsciiConverter(ch);
-            if (number is < 0 or >= Modulo)
-            {
-                throw new InvalidTokenException($"Character '{ch}' is not valid in Mod 97,10 calculation.");
-            }
+            Debug.Assert(
+                number is >= 0 and < Modulo,
+                $"Value {number} for character '{ch}' is not valid in Mod 97,10 calculation."
+            );
 
             // If the number we got is a two-digit number (i.e. >= 10) we need to shift left by 100, else by 10.
-            int base10Shift = number < 10 ? 10 : 100;
-            remainder = (remainder * base10Shift + number) % Modulo;
+            int numberDigits = 2;
+            int base10Shift = 100;
+            if (number < 10)
+            {
+                numberDigits = 1;
+                base10Shift = 10;
+            }
+
+            remainder = remainder * base10Shift + number;
+            if (digits + numberDigits >= MaxDigitsBeforeIntegerOverflow)
+            {
+                // Compute the new remainder to avoid integer overflow and reset the number of digits.
+                remainder %= Modulo;
+                digits = remainder < 10 ? 1 : 2;
+            }
+            else
+            {
+                digits += numberDigits;
+            }
+        }
+
+        if (remainder >= Modulo)
+        {
+            remainder %= Modulo;
         }
 
         return _complement(Modulo, remainder);
---
+++ test/IbanNet.Extensions.Bban.Benchmark/AlgorithmBenchmarks.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
+using IbanNet.CheckDigits.Calculators;
+using IbanNet.Extensions.Bban.CheckDigits;
+using IbanNet.Extensions.Bban.CheckDigits.Algorithms;
+
+namespace IbanNet.Extensions.Bban.Benchmark;
+
+[SimpleJob(RuntimeMoniker.Net80, baseline: true)]
+[SimpleJob(RuntimeMoniker.Net48)]
+[MarkdownExporterAttribute.GitHub]
+[Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+[MemoryDiagnoser]
+[InliningDiagnoser(false, ["IbanNet.Extensions.Bban"])]
+public class AlgorithmBenchmarks
+{
+    private readonly Mod97CheckDigitsCalculator _mod9710Calculator = new();
+    private readonly Mod9710Algorithm _mod9710 = new(Complement.Remainder);
+    private char[] _alphaNumericData = null!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        // Note: for fair comparisons, the same data (and length) is used for all numeric (digits only) and alphanumeric (mixed) algorithms respectively.
+        _alphaNumericData = "BA3912900e9401Z28494".ToCharArray();
+    }
+
+    [Benchmark(Description = "ISO 7064 MOD 97-10")]
+    public void Mod9710()
+    {
+        _mod9710.Compute(_alphaNumericData);
+    }
+
+    [Benchmark(Description = "ISO 7064 MOD 97-10 (IbanNet library)", Baseline = true)]
+    public void CoreMod9710()
+    {
+        _mod9710Calculator.Compute(_alphaNumericData);
+    }
+}
