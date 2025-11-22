9f08e8a5b51dc864c3065787f8ba1cee9cfe92c9
skwasjer <11424653+skwasjer@users.noreply.github.com>
2025-11-22 07:27:09 +0100

    perf: improve Luhn algorithm performance.
    - removes the modulo operation while iterating chars
    - does not expect a fixed length input
    - starts at last char and moves to beginning as defined in the spec (the previous impl. only worked with inputs of odd length)

--- test/IbanNet.Extensions.Bban.Tests/CheckDigits/Algorithms/LuhnAlgorithmTests.cs
+++ test/IbanNet.Extensions.Bban.Tests/CheckDigits/Algorithms/LuhnAlgorithmTests.cs
@@ -1,48 +1,31 @@
 ﻿namespace IbanNet.Extensions.Bban.CheckDigits.Algorithms;
 
-public class LuhnAlgorithmTests
+public sealed class LuhnAlgorithmTests
 {
-    private readonly LuhnAlgorithm _sut;
+    private readonly LuhnAlgorithm _sut = new();
 
-    public LuhnAlgorithmTests()
+    [Theory]
+    [InlineData("1789372997", 4)] // From Wikipedia
+    [InlineData("1009300012345", 8)] // From IBAN FI1410093000123458
+    [InlineData("1234567890123", 7)] // Test case
+    [InlineData("0000000000000", 0)] // Edge case: all zeros
+    [InlineData("1990000777770", 9)] // Test case
+    [InlineData("937541841561453546570540148077931", 9)] // Long number
+    public void It_should_return_expected(string input, int expectedResult)
     {
-        _sut = new LuhnAlgorithm();
+        _sut.Compute(input).Should().Be(expectedResult);
     }
 
     [Theory]
-    [InlineData("1009300012345", 8)]  // From IBAN FI1410093000123458
-    [InlineData("1234567890123", 7)]  // Test case
-    [InlineData("0000000000000", 0)]  // Edge case: all zeros
-    [InlineData("1990000777770", 9)]  // Test case
-    public void Given_first_thirteen_digits_when_computing_check_digit_should_match_expected(string firstThirteenDigits, int expectedCheckDigit)
+    [InlineData("123:5123451234567891", ':', 3)]
+    [InlineData("123451234512345é7891", 'é', 15)]
+    [InlineData("123451234a1234567891", 'a', 9)]
+    [InlineData("B2345123451234567891", 'B', 0)]
+    public void Given_that_input_contains_a_non_numeric_character_when_computing_it_should_throw(string value, char invalidChar, int errorPosition)
     {
-        // Act
-        int actual = _sut.Compute(firstThirteenDigits);
-
-        // Assert
-        actual.Should().Be(expectedCheckDigit);
-    }
-
-    [Fact]
-    public void Given_non_numeric_input_when_computing_should_throw_InvalidTokenException()
-    {
-        // Arrange
-        const string invalidInput = "ABC1234567890";
-
-        // Act & Assert
-        _sut.Invoking(c => c.Compute(invalidInput))
-            .Should().Throw<InvalidTokenException>()
-            .WithMessage("*numeric*");
-    }
-
-    [Theory]
-    [InlineData("123456789012")]   // Too short (12 digits)
-    [InlineData("12345678901234")] // Too long (14 digits)
-    public void Given_invalid_length_when_computing_should_throw_InvalidTokenException(string invalidInput)
-    {
-        // Act & Assert
-        _sut.Invoking(c => c.Compute(invalidInput))
-            .Should().Throw<InvalidTokenException>()
-            .WithMessage("*13 digits*");
+        _sut.Invoking(s => s.Compute(value))
+            .Should()
+            .Throw<InvalidTokenException>()
+            .WithMessage($"Expected numeric character at position {errorPosition}, but found '{invalidChar}'.");
     }
 }
