187348912b5aead45afeb6b630f978457456b1d9
skwasjer <11424653+skwasjer@users.noreply.github.com>
2025-11-23 15:12:19 +0100

    refactor: moves the buffer to parent ns, and adds index range check for safety

--- src/IbanNet.Extensions.Bban/CheckDigits/Algorithms/CheckDigitsAlgorithm.cs
+++ src/IbanNet.Extensions.Bban/CheckDigits/Algorithms/CheckDigitsAlgorithm.cs
@@ -7,19 +7,12 @@ internal abstract class CheckDigitsAlgorithm : ICheckDigitsAlgorithm
 #if USE_SPANS
     public int Compute(ReadOnlySpan<char> value)
     {
+#else
+    public int Compute(string value)
+    {
+#endif
         return Compute(Buffer.Create(value));
     }
-#else
-    public unsafe int Compute(string value)
-    {
-        // ReSharper disable once NullCoalescingConditionIsAlwaysNotNullAccordingToAPIContract
-        value ??= string.Empty;
-        fixed (char* ptr = value)
-        {
-            return Compute(Buffer.Create(ptr, value.Length));
-        }
-    }
-#endif
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     protected abstract int Compute(Buffer buffer);
