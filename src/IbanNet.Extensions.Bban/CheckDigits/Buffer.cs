c13d6702fba4db315a1fcee2af8e4b386591eb3e
Martijn Bodeman <11424653+skwasjer@users.noreply.github.com>
2025-11-24 03:37:57 +0100

    refactor: remove unsafe code (#111)

--- src/IbanNet.Extensions.Bban/CheckDigits/Buffer.cs
+++ src/IbanNet.Extensions.Bban/CheckDigits/Buffer.cs
@@ -2,9 +2,9 @@
 
 namespace IbanNet.Extensions.Bban.CheckDigits;
 
-#if USE_SPANS
 internal readonly ref struct Buffer
 {
+#if USE_SPANS
     private readonly ReadOnlySpan<char> _value;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -12,6 +12,28 @@ internal readonly ref struct Buffer
     {
         _value = value;
     }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Buffer Create(ReadOnlySpan<char> value)
+    {
+        return new Buffer(value);
+    }
+#else
+    private readonly string _value;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private Buffer(string value)
+    {
+        _value = value;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Buffer Create(string? value)
+    {
+        value ??= string.Empty;
+        return new Buffer(value);
+    }
+#endif
 
     public int Length
     {
@@ -24,72 +46,9 @@ internal readonly ref struct Buffer
         return _value.ToString();
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Buffer Create(ReadOnlySpan<char> value)
-    {
-        return new Buffer(value);
-    }
-
     public char this[int index]
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => _value[index];
     }
 }
-#else
-internal readonly unsafe ref struct Buffer
-{
-    private readonly char* _value;
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private Buffer(char* value, int length)
-    {
-        _value = value;
-        Length = length;
-    }
-
-    public int Length
-    {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        get;
-    }
-
-    public override string ToString()
-    {
-        return new string(_value, 0, Length);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Buffer Create(string? value)
-    {
-        value ??= string.Empty;
-        fixed (char* ptr = value)
-        {
-            return new Buffer(ptr, value.Length);
-        }
-    }
-
-    public char this[int index]
-    {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        get
-        {
-            if (index < 0 || index >= Length)
-            {
-                // Disabling warnings as this is the intended exception to throw in this case,
-                // since we're accessing an indexer out of the bounds of the buffer.
-                // We acknowledge that throwing IndexOutOfRangeException directly is generally discouraged,
-                // but in this specific context, it is appropriate, esp. because (ReadOnly)Span<T> does the same,
-                // and we want the buffer to behave similarly in all TFM's.
-#pragma warning disable CA2201
-#pragma warning disable S112
-                throw new IndexOutOfRangeException();
-#pragma warning restore S112
-#pragma warning restore CA2201
-            }
-
-            return _value[index];
-        }
-    }
-}
-#endif
